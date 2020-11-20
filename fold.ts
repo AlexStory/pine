642615c3290bfff0c2a15f1770142694273e6816
alex story <astory11@yahoo.com>
2020-11-20 11:01:15 -0600

    redid fold logic

--- fold.ts
+++ fold.ts
@@ -1,9 +1,9 @@
-import { length } from "./mod.ts";
-
 export function fold<T, U>(fn: (acc: U, x: T) => U, acc: U, list: T[]): U {
   let result = acc;
-  for (let i = 0; i < length(list); i++) {
-    result = fn(result, list[i]);
+
+  for (const item of list) {
+    result = fn(result, item);
   }
+
   return result;
 }
